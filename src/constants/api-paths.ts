5c1256fe576ffdfc55dbdb5d067a67c93d7fb632
Artem Farafonov <artem_farafonov@epam.com>
2023-03-05 00:11:02 +0300

    update endpoints

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,5 +1,7 @@
 export const API_PATHS = {
-	product: 'https://j0qlfgbxi0.execute-api.us-east-2.amazonaws.com/products',
+	product: 'https://86rq1or8mj.execute-api.us-east-2.amazonaws.com/products',
+	productAvailable:
+		'https://86rq1or8mj.execute-api.us-east-2.amazonaws.com/products/available',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
