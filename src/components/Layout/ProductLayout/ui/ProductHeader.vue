5ed3c16012123b49b7322c6d5a34a18625d450c2
Artem Farafonov <artem_farafonov@epam.com>
2023-03-25 17:52:08 +0300

    task-7: login/logout, authorized import

--- src/components/Layout/ProductLayout/ui/ProductHeader.vue
+++ src/components/Layout/ProductLayout/ui/ProductHeader.vue
@@ -8,6 +8,14 @@
 
 		<v-spacer></v-spacer>
 
+		<v-btn data-test-id="logout" icon large @click="logout()">
+			<v-icon>mdi-logout-variant</v-icon>
+		</v-btn>
+
+		<v-btn data-test-id="login" icon large @click="setSampleLogin()">
+			<v-icon>mdi-login-variant</v-icon>
+		</v-btn>
+
 		<v-account-btn-menu></v-account-btn-menu>
 
 		<v-btn data-test-id="cart-link" to="/cart" icon large>
@@ -37,6 +45,18 @@ export default Vue.extend({
 		cartCountBadge: Number,
 	},
 	components: { VAccountBtnMenu, VLanguagePickerBtnMenu },
+	methods: {
+		setSampleLogin: () => {
+			const defaultPair = 'farafonoff:TEST_PASSWORD';
+			alert(`Using default pair: ${defaultPair}`);
+			const encoded = Buffer.from(defaultPair).toString('base64');
+			localStorage.setItem('authorization_token', encoded);
+		},
+		logout: () => {
+			localStorage.removeItem('authorization_token');
+			alert('logged out');
+		},
+	},
 });
 </script>
 
